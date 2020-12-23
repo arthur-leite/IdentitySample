8adb37d5317cda633b777ca3628c92dfcf4be631
Arthur Leite <arthur.o.leite@gmail.com>
2020-12-23 20:49:43 -0300

    Add Context class and IdentityDbContext references

---
+++ IdentitySample/Context.cs
@@ -0,0 +1,11 @@
+﻿using IdentitySample.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity
+{
+    public class Context : IdentityDbContext<User, Role, int>
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+    }
+}
